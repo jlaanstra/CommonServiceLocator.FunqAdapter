efda691e9076e20c15358096210e7eb89159df4d
Johan Laanstra <jlaanstra1221@hotmail.com>
2012-10-29 13:48:16 +0100

    Added some info.

--- Microsoft.Practices.ServiceLocation.FunqAdapter/Properties/AssemblyInfo.cs
+++ Microsoft.Practices.ServiceLocation.FunqAdapter/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CommonServiceLocator.FunqAdapter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ServiceLocator adapter for the Funq DI container.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Johan Laanstra")]
 [assembly: AssemblyProduct("CommonServiceLocator.FunqAdapter")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
